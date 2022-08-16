fcbe0e24b880e1476f3d0666a3d2de914a5fbbb0
awadh <awadh.hassan@student.moringaschool.com>
2022-08-17 00:22:33 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(work = "roller-skate"){
+    
+    return`This Saturday, I want to ${work}!`;
+}
+saturdayFun();
+
+
+console.log ("This monday,i will" ,mondayWork());
+
+function mondayWork(work = "go to the office"){
+    return`This Monday, I will ${work}.`
+
+}
+mondayWork();
+
+function wrapAdjective(visual="*"){
+  return function(adjective = "special"){
+    return `You are ${visual}${adjective}${visual}!`
+
+  }
+  
+}
+wrapAdjective();
